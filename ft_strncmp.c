ef4bdd7469384518117feca5486c2714fca8c83f
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-10-29 08:44:06 +0200

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,22 +6,22 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 16:36:34 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/10/28 16:46:49 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/10/29 08:43:44 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
-	int				result;
+	size_t	i;
 
 	i = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		result = s1[i] - s2[i];
-		if (result != 0)
+		if (s1[i] != s2[i])
 		{
-			return (result);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
