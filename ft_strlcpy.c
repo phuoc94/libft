37c271b18eee24b30fbadb47b2f7c292fe4de09e
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-10-31 06:36:25 +0200

    fix strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 16:36:30 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/10/30 16:09:42 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/10/31 06:34:55 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	i;
+	size_t i;
+	size_t src_len;
 
 	i = 0;
-	while (src[i] && i < size)
+	src_len = ft_strlen(src);
+
+	if (size == 0)
+		return (src_len);
+
+	while (src[i] && i < size - 1)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i + 1] = '\0';
-	return (i);
-}
+	dst[i] = '\0';
 
+	return (src_len);
+}
