1057f5ca6b077d1016b383837f77cba32722bd16
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-11-01 19:43:43 +0200

    fix strjoin

--- tests/test_ft_strjoin.c
+++ tests/test_ft_strjoin.c
@@ -6,22 +6,22 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:34:32 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/10/31 11:37:44 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/01 19:06:50 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft.h"
 #include <stdio.h>
 
 int main(void)
 {
 	char *ptr;
-	char s1[] = "hello";
-	char s2[] = "world";
+	char s1[] = "lorem ipsum";
+	char s2[] = "\0";
 
 	ptr = ft_strjoin(s1, s2);
 
-	printf("p: %s", ptr);
+	printf("p:%s", ptr);
 
 	return (0);
 }
