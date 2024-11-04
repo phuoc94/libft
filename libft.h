12b588c3214e026abc45b76bb072cffe33d8e105
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-11-04 14:00:41 +0200

    done ft_lstdelone

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:53:10 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/11/04 13:41:15 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:54:58 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,6 @@ void			ft_lstadd_front(t_list **lst, t_list *new);
 int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
 void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void*));
 
 #endif
