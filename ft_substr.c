46588fd420f0df403320884a81dbd6c92e7fb37a
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-11-05 22:00:27 +0200

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 10:32:16 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/11/05 15:31:14 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/05 21:54:21 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*sub_s;
 	size_t	s_len;
 
+	if (!s)
+		return (NULL);
 	s_len = ft_strlen(s);
-	sub_s = malloc(sizeof(char) * len + 1);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (start + len > s_len)
+		len = s_len - start;
+	sub_s = malloc(sizeof(char) * (len + 1));
 	if (!sub_s)
 		return (NULL);
-	if (start >= s_len)
-	{
-		sub_s[0] = '\0';
-		return (sub_s);
-	}
 	ft_strlcpy(sub_s, &s[start], len + 1);
 	return (sub_s);
 }
