faad00add99adea4c10ef2351a0ea9ab30c2a14b
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-11-05 14:54:44 +0200

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 16:36:34 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/11/01 17:51:37 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/05 14:54:31 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && (s1[i] || s2[i]))
 	{
 		if (s1[i] != s2[i])
 		{
