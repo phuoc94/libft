ceb74d5eff448b4f47049fbfbb71612297b3ed60
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-11-05 18:53:39 +0200

    formatiing calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 08:30:53 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/11/05 18:42:43 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/05 18:52:38 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,12 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 	if (nmemb == 0 || size == 0)
 		return (malloc(1));
-
 	total_size = nmemb * size;
 	if (total_size / size != nmemb)
 		return (NULL);
-
 	ptr = malloc(total_size);
 	if (!ptr)
 		return (NULL);
-
 	ft_memset(ptr, 0, total_size);
 	return (ptr);
 }
