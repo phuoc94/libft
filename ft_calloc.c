63a7ff5cf8564d6832cf91c2cf2c3820c86d371d
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-11-07 19:55:48 +0200

    fix ft_calloc to return NULL for zero allocation requests

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 08:30:53 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/11/05 18:52:38 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/07 19:55:33 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	if (nmemb == 0 || size == 0)
-		return (malloc(1));
+		return (NULL);
 	total_size = nmemb * size;
 	if (total_size / size != nmemb)
 		return (NULL);
